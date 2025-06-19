756c7edadce38cd782f4bc1d9c56f4fccc01a2b7
Beenie1234 <cannonsven@gmail.com>
2025-06-19 12:07:46 +0200

    Working read_time_and_length

--- analysis_driver.py
+++ analysis_driver.py
@@ -54,24 +54,65 @@ def open_edf_file(edf_path):
         logging.error(f"Error opening EDF file: {e}")
         raise
 
-def read_time_and_length():
-    logging.info("Starting to read time and length")
-    screenshot = ImageGrab.grab()
 
-    time_label = pyautogui.locateOnScreen("assets/images/time_label.png", confidence=0.8)
-    length_label = pyautogui.locateOnScreen("assets/images/time_label.png", confidence=0.8)
-    if time_label and length_label:
-        print("IMAGES FOUND")
-    else:
-        print("images could not be found")
+
+def read_time_and_length():
+    logging.info("Starting to read time and length with Tesseract-OCR")
+    time.sleep(1)
+    screen_w, screen_h = pyautogui.size()
+
+    screenshot = ImageGrab.grab()
+    try:
+        time_label = pyautogui.locateOnScreen("assets/images/time_label.png", confidence=0.8)
+        length_label = pyautogui.locateOnScreen("assets/images/length_label.png", confidence=0.8)
+
+        if not time_label and length_label:
+            logging.error("Time or length label not found on screen")
+            return None, None
+
+
+
+
+        time_region =(
+            int(time_label.left + time_label.width ),
+            int(time_label.top),
+            int(time_label.left + time_label.width + 45),
+            int(time_label.top + time_label.height)
+        )
+        length_region = (
+            int(length_label.left + length_label.width ),
+            int(length_label.top),
+            int(length_label.left + length_label.width + 50),
+            int(length_label.top + length_label.height)
+        )
+        print(f"OCR_region for time: {time_region}. Length: {length_region}")
+        screenshot_time = ImageGrab.grab(bbox=time_region).convert('L')
+        screenshot_length = ImageGrab.grab(bbox=length_region).convert('L')
+        time_text = pytesseract.image_to_string(screenshot_time).strip()
+        length_text = pytesseract.image_to_string(screenshot_length).strip()
+
+
+        try:
+            print(f"Time text: {time_text}")
+            print(f"Length text: {length_text}")
+            return time_text, length_text
+        except Exception as e:
+            logging.error(f"Could not extract time text from image: {e}")
+        return None, None
+
+
+    except Exception as e:
+        print(str(e))
+        return None, None
 
 
 if __name__ == "__main__":
 
-    open_kubios(r"C:\Program Files\Kubios\KubiosHRVScientific\application\launch_kubioshrv.exe")
-    edf_file_path = resolve_edf_paths(EXCEL_PATH, read_edf_list(EXCEL_PATH))
-    open_edf_file(edf_file_path[0])
-    detect_analysis_error()
-
+    #open_kubios(r"C:\Program Files\Kubios\KubiosHRVScientific\application\launch_kubioshrv.exe")
+    #edf_file_path = resolve_edf_paths(EXCEL_PATH, read_edf_list(EXCEL_PATH))
+    #open_edf_file(edf_file_path[0])
+    #detect_analysis_error()
+    time.sleep(4)
+    read_time_and_length()
 
 
